2048e0a99202524cecf4b94bf126dbbdaa44132d
Ákos Nikházy <nikhazy.akos@gmail.com>
2021-11-11 14:39:31 +0100

    Add files via upload

---
+++ blog/codestuff/scripts.js
@@ -0,0 +1,39 @@
+"use strict"
+var lights = 1;
+
+const lightswitch = document.getElementById('darkmode');
+const onoff = lightswitch.firstElementChild;
+const html 	= document.getElementsByTagName('html');
+const imgTags = document.querySelectorAll('img');
+
+function invertImgs(l){
+	if(1 == l)
+	{
+		imgTags.forEach(function(item,i){
+			imgTags[i].classList.add('lightsoff');
+		});
+		return;
+	}
+	
+	imgTags.forEach(function(item,i){
+		imgTags[i].classList.remove('lightsoff');
+	});
+}
+
+lightswitch.addEventListener("click", function()
+{
+	if(1 == lights)
+	{
+		onoff.textContent = "on";
+		html[0].classList.add('lightsoff');
+		invertImgs(1);
+		lights = 0;
+		return;
+	}
+	
+	onoff.textContent = "off";
+	html[0].classList.remove('lightsoff');
+	invertImgs(0);
+	lights = 1;
+			
+});
