060ff73766a0adcf31f2ac752e36387fa4664d5b
Ákos Nikházy <nikhazy.akos@gmail.com>
2022-05-31 08:00:23 +0200

    Delete teszt directory

--- teszt/computerThoughts/mind.js
+++
@@ -1,8 +0,0 @@
-/***********************
-	Nikházy Ákos
-
-index.js
-
-itt indul el a program
-***********************/
-'use strict';ReactDOM.createRoot(document.querySelector('#formContainer')).render(<Form/>);
